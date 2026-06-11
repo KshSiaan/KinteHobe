c08313d8bc4664805658b395e9b3af1d17e80c31
KshSiaan <kshsiaan@gmail.com>
2026-06-11 12:32:19 +0600

    quick

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,7 +10,7 @@ export const auth = betterAuth({
     }),
     secret: process.env.BETTER_AUTH_SECRET,
     baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
-    trustedOrigins: ["http://localhost:3000"],
+    trustedOrigins: ["https://kintehobe.vercel.app","http://localhost:3000"],
     emailAndPassword: {
         enabled: true,
         requireEmailVerification: true,
