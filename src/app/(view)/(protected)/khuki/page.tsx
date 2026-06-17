6c96a204b6fa061d97dc023f52a4ab8242ec9a22
KshSiaan <kshsiaan@gmail.com>
2026-06-17 12:13:07 +0600

    quick

---
+++ src/app/(view)/(protected)/khuki/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <main className="px-4 h-[80dvh] py-4 grid grid-cols-4 gap-6">
+      <div className="border h-full"></div>
+      <div className="col-span-3"></div>
+    </main>
+  );
+}
