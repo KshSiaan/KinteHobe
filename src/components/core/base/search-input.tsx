4eb5a2ce0c71f7e7b436c4e3b64de0adbf008474
KshSiaan <kshsiaan@gmail.com>
2026-08-30 20:05:53 +0600

    quick

--- src/components/core/base/search-input.tsx
+++ src/components/core/base/search-input.tsx
@@ -16,6 +16,8 @@ import { howl } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { CreateResponseType } from "@/lib/backend/message";
+import { Spinner } from "@/components/kibo-ui/spinner";
 export default function SearchInput() {
   const [open, setOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
@@ -44,7 +46,11 @@ export default function SearchInput() {
     };
   }, []);
 
-  const { data } = useQuery({
+  const {
+    data,
+    isEnabled,
+    isPending: isSearchPending,
+  } = useQuery({
     queryKey: ["search", debouncedSearchQuery],
     enabled: !!debouncedSearchQuery,
     queryFn: async (): Promise<{
@@ -81,6 +87,92 @@ export default function SearchInput() {
       }>;
     }> => {
       return howl(`/api/client/search?q=${debouncedSearchQuery}`);
+    },
+  });
+
+  const { data: dailyDiscoverData } = useQuery({
+    queryKey: ["dailyDiscover"],
+    queryFn: async (): Promise<
+      CreateResponseType<{
+        data: {
+          id: string;
+          slug: string;
+          title: string;
+          description: string;
+          category: {
+            id: string;
+            name: string;
+            slug: string;
+            description: string;
+            image: string;
+            banner: string;
+            isActive: boolean;
+            metaTitle: string;
+            metaDescription: string;
+            createdAt: string;
+            updatedAt: string;
+          };
+          categoryId: string;
+          status: string;
+          variantIds: Array<string>;
+          createdAt: string;
+          updatedAt: string;
+          variants: Array<{
+            id: string;
+            groupId: string;
+            code?: string;
+            sku: string;
+            price: string;
+            compareAtPrice: string;
+            stockQuantity: number;
+            weight?: string;
+            details: string;
+            metadata: Array<{
+              id: string;
+              name: string;
+              description: string;
+            }>;
+            position: number;
+            kind: string;
+            enabled: boolean;
+            title: string;
+            optionName: any;
+            images: Array<string>;
+            createdAt: string;
+            updatedAt: string;
+            publicImages: Array<string>;
+          }>;
+        }[];
+      }>
+    > => {
+      const res = await fetch("/api/product?limit=6");
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  const { data: cats } = useQuery({
+    queryKey: ["fetchCategories"],
+    queryFn: async (): Promise<
+      CreateResponseType<{
+        data: {
+          id: string;
+          name: string;
+          slug: string;
+          image: string;
+          banner: string;
+          description: string;
+          isActive: boolean;
+          metaTitle: string;
+          metaDescription: string;
+          createdAt: string;
+          updatedAt: string;
+        }[];
+      }>
+    > => {
+      const res = await fetch("/api/client/category");
+      if (!res.ok) throw new Error("Failed to fetch categories");
+      return res.json();
     },
   });
 
@@ -179,21 +271,22 @@ export default function SearchInput() {
               </div>
 
               <div className="space-y-2">
-                {data?.data?.map((item, i) => (
-                  <motion.button
-                    key={item.id}
-                    initial={{
-                      opacity: 0,
-                      x: -10,
-                    }}
-                    animate={{
-                      opacity: 1,
-                      x: 0,
-                    }}
-                    transition={{
-                      delay: i * 0.05,
-                    }}
-                    className="
+                {isEnabled &&
+                  data?.data.map((item, i) => (
+                    <motion.button
+                      key={item.id}
+                      initial={{
+                        opacity: 0,
+                        x: -10,
+                      }}
+                      animate={{
+                        opacity: 1,
+                        x: 0,
+                      }}
+                      transition={{
+                        delay: i * 0.05,
+                      }}
+                      className="
                       flex
                       w-full
                       items-center
@@ -206,24 +299,97 @@ export default function SearchInput() {
                       transition-colors
                       hover:bg-accent
                     "
-                  >
-                    {item.publicImages?.[0] && (
-                      <Image
-                        src={item?.publicImages[0] || "/images/placeholder.png"}
-                        alt={item.title}
-                        className="size-12 rounded-md object-cover"
-                        height={48}
-                        width={48}
-                      />
-                    )}
-                    <div className="">
-                      <h4>{item.title}</h4>
-                      <p className="text-xs text-muted-foreground line-clamp-1">
-                        {item.details}
-                      </p>
+                    >
+                      {item.publicImages?.[0] && (
+                        <Image
+                          src={
+                            item?.publicImages[0] || "/images/placeholder.png"
+                          }
+                          alt={item.title}
+                          className="size-12 rounded-md object-cover"
+                          height={48}
+                          width={48}
+                        />
+                      )}
+                      <div className="">
+                        <h4>{item.title}</h4>
+                        <p className="text-xs text-muted-foreground line-clamp-1">
+                          {item.details}
+                        </p>
+                      </div>
+                    </motion.button>
+                  ))}
+
+                {data?.data?.length === 0 && <div>No results found</div>}
+                {!isEnabled && isSearchPending && (
+                  <div className="border-y py-2">
+                    <div className="grid grid-cols-4 gap-4">
+                      {cats?.data
+                        ?.sort(() => Math.random() - 0.5)
+                        .slice(0, 4)
+                        .map((cat) => (
+                          <Button key={cat.id} variant="ghost">
+                            {cat.name}
+                          </Button>
+                        ))}
                     </div>
-                  </motion.button>
-                ))}
+                  </div>
+                )}
+
+                {!isEnabled &&
+                  dailyDiscoverData?.data?.map((item, i) => (
+                    <motion.button
+                      key={item.id}
+                      initial={{
+                        opacity: 0,
+                        x: -10,
+                      }}
+                      animate={{
+                        opacity: 1,
+                        x: 0,
+                      }}
+                      transition={{
+                        delay: i * 0.05,
+                      }}
+                      className="
+                      flex
+                      w-full
+                      items-center
+                      gap-3
+                      rounded-xl
+                      px-3
+                      py-2.5
+                      text-left
+                      text-sm
+                      transition-colors
+                      hover:bg-accent
+                    "
+                    >
+                      {item.variants[0].publicImages && (
+                        <Image
+                          src={
+                            item?.variants[0].publicImages[0] ||
+                            "/images/placeholder.png"
+                          }
+                          alt={item.title}
+                          className="size-12 rounded-md object-cover"
+                          height={48}
+                          width={48}
+                        />
+                      )}
+                      <div className="">
+                        <h4>{item.title}</h4>
+                        <p className="text-xs text-muted-foreground line-clamp-1">
+                          {item.variants[0].details}
+                        </p>
+                      </div>
+                    </motion.button>
+                  ))}
+                {isEnabled && isSearchPending && (
+                  <div className="flex justify-center items-center p-4">
+                    <Spinner variant="bars" className="text-primary" />
+                  </div>
+                )}
               </div>
             </div>
           </motion.div>
